40bb2182133a1d504bfc2038a5f842b1fbec170f
Mohit Jeswani <mohitjeswani1210@gmail.com>
2023-09-23 12:49:25 +0530

    23-09-2023 - Final for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
+import Sections from "./components/Sections";
+import Axios from "axios";
+import Homide from "./assets/Homide.jpg";
+import Milflox from "./assets/Milflox.jpg";
+import Pred_Forte from "./assets/Pred_Forte.jpg";
+
+import "./app.css";
+import { useEffect, useState } from "react";
+
 const App = () => {
+  // Use State for the Data
+  const [dropInfo, setDropInfo] = useState([]);
+  // Use State for update trigger
+  const [updateTrigger, setUpdateTrigger] = useState(false);
+
+  // Use Effect to fetch the data
+  useEffect(() => {
+    // Fetch drop information when the component mounts
+    fetchDropInfo().then((data) => setDropInfo(data));
+    console.log(dropInfo);
+  }, [updateTrigger, dropInfo]);
+
+  // Define a function to update drop information
+  const updateDrop = async (name, count) => {
+    try {
+      // Send a POST request to the back-end to update drop information
+      await Axios.post("http://localhost:3500/update-drop", { name, count });
+      setUpdateTrigger(!updateTrigger);
+    } catch (error) {
+      console.error("Error updating drop:", error);
+    }
+  };
+
+  // Define a function to fetch drop information
+  const fetchDropInfo = async () => {
+    try {
+      // Send a GET request to the back-end to fetch drop information
+      const response = await Axios.get("http://localhost:3500/get-drop-info");
+      return response.data; // Assuming the response contains drop information
+    } catch (error) {
+      console.error("Error fetching drop information:", error);
+      return []; // Return an empty array in case of an error
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello, world!</p>
+        <h1>Week-1</h1>
+        <h1>Day-3</h1>
       </header>
+      <main className="App-main">
+        {dropInfo.map((info, index) => {
+          let times, imgUrl;
+
+          if (info.name === "Homide") {
+            times = 2;
+            imgUrl = Homide;
+          } else if (info.name === "Milflox") {
+            times = 4;
+            imgUrl = Milflox;
+          } else {
+            times = 8;
+            imgUrl = Pred_Forte;
+          }
+          return <Sections key={index} times={times} name={info.name} imgUrl={imgUrl} done={info.count} buttonClick={() => updateDrop(info.name, info.count + 1)} />;
+        })}
+      </main>
     </div>
   );
 };
--- src/components/Sections.js
+++ src/components/Sections.js
@@ -1,5 +1,37 @@
-const Sections = () => {
-  return <div className="sections"></div>;
+import "./components.css";
+
+const Checks = ({ num, cla }) => {
+  return (
+    <div className={`checks ${cla}`}>
+      <p>{num}</p>
+      <div></div>
+    </div>
+  );
+};
+
+const Sections = ({ name, done, times, imgUrl, buttonClick }) => {
+  const checksArray = Array.from({ length: times }, (_, index) => <Checks key={index} num={index + 1} cla={index < done ? "done" : "left"} />);
+
+  return (
+    <div className="sections">
+      <div className="top">
+        <h3 className="top_name">{name}</h3>
+        <h3 className="top_times">
+          <span className="top_times-left"> {done} /</span>
+          <span className="top_times-done"> {times}</span>
+        </h3>
+      </div>
+      <div className="body">
+        <img src={imgUrl} alt="Homide" />
+        <div className="body_checklist">
+          <button className="body_checklist-button" onClick={buttonClick}>
+            +
+          </button>
+          <div className="body_checklist-checks">{checksArray}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sections;
