7ad9c47f06879f3d50f0cfe9e75197704de1acef
Mohit Jeswani <mohitjeswani1210@gmail.com>
2023-09-25 12:18:13 +0530

    Added last update and weeks too

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import { useEffect, useState } from "react";
 const App = () => {
   // Use State for the Data
   const [dropInfo, setDropInfo] = useState([]);
+  const [showtime, setShowtime] = useState("");
   const baseURL = "https://drops-back.vercel.app";
+  let week = 0;
 
   // Define a function to format the date
   function formatDate(date) {
@@ -20,14 +22,53 @@ const App = () => {
   const currentDate = new Date();
   const formattedDate = formatDate(currentDate);
 
+  function dateDiffInDays(dateString1, dateString2) {
+    const date1 = new Date(dateString1);
+    const date2 = new Date(dateString2);
+
+    // Calculate the time difference in milliseconds
+    const timeDiff = date2 - date1;
+
+    // Convert milliseconds to days
+    const daysDiff = timeDiff / (1000 * 3600 * 24);
+
+    return Math.abs(Math.floor(daysDiff)); // Use Math.abs to get a positive value
+  }
+
+  const startDate = new Date("September 21, 2023");
+
+  week = Math.round(dateDiffInDays(startDate, currentDate) / 7);
+
+  const options = {
+    hour: "numeric", // "numeric", "2-digit"
+    minute: "numeric", // "numeric", "2-digit"
+    hour12: true, // true or false
+  };
+  const currentTime = new Intl.DateTimeFormat("en-US", options).format(currentDate);
+
   // Define a function to update drop information
-  const updateDrop = async (name, count) => {
+  const updateDrop = async (name, count, showTime) => {
     try {
       // Send a POST request to the back-end to update drop information
+      console.log("POST Request", name, count, showTime);
       await Axios.post(`${baseURL}/update-drop`, { name, count, formattedDate });
-      fetchDropInfo(formattedDate).then((data) => setDropInfo(data));
+      await Axios.post(`${baseURL}/update-showtime`, { showTime: showTime });
+      await fetchDropInfo(formattedDate).then((data) => setDropInfo(data));
+      await fetchShowTime().then((data) => setShowtime(data));
     } catch (error) {
       console.error("Error updating drop:", error);
+    }
+  };
+
+  // Define a function to fetch drop information
+  const fetchShowTime = async () => {
+    try {
+      const response = await Axios.get(`${baseURL}/get-showtime`);
+      console.log(response.data.time);
+      return response.data.time; // Assuming the response contains the showTime
+    } catch (error) {
+      console.error("Error fetching showTime:", error);
+      return "";
     }
   };
 
@@ -48,14 +89,22 @@ const App = () => {
   useEffect(() => {
     // Fetch drop information when the component mounts
     fetchDropInfo(formattedDate).then((data) => setDropInfo(data));
+    fetchShowTime().then((data) => setShowtime(data));
   }, [formattedDate]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Week-1</h1>
-        <h1>Day-3</h1>
+        <div className="time">
+          <h2>Week No.</h2>
+          <h1>{week}</h1>
+        </div>
+        <div className="time">
+          <h2>Last updated</h2>
+          <h1>{showtime}</h1>
+        </div>
       </header>
+
       <main className="App-main">
         {dropInfo?.map?.((info, index) => {
           let times, imgUrl;
@@ -70,7 +119,12 @@ const App = () => {
             times = 8;
             imgUrl = Pred_Forte;
           }
-          return <Sections key={index} times={times} name={info.name} imgUrl={imgUrl} done={info.count} buttonClick={() => updateDrop(info.name, info.count + 1)} />;
+
+          if (week > 1 && info.name === "Homide") {
+            return;
+          } else if (week > 4 && info.name === "Milflox") {
+            return;
+          } else return <Sections key={index} times={times} name={info.name} imgUrl={imgUrl} done={info.count} buttonClick={() => updateDrop(info.name, info.count + 1, currentTime)} />;
         })}
       </main>
     </div>
