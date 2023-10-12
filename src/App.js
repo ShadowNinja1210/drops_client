5467357bff6f76bd24f31228e07ccf71751c048e
Mohit Jeswani <mohitjeswani1210@gmail.com>
2023-10-12 16:17:53 +0530

    Changed something with the time. It will now take the updated time. Now, we don't have to create new drops manually. The time was not working. Fixed it too.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Axios from "axios";
 import Vigamox from "./assets/Vigamox.jpg";
 import Optive from "./assets/Optive.jpg";
 import Pred_Forte from "./assets/Pred_Forte.jpg";
-
 import "./app.css";
 import { useEffect, useState } from "react";
 
 const App = () => {
+  // --------------------------------------------------------------- //
   // Use State for the Data
   const [dropInfo, setDropInfo] = useState([]);
   const [showtime, setShowtime] = useState("");
-  const [currentTime, setCurrentTime] = useState("");
   const baseURL = "https://drops-back.vercel.app";
   let day_num = 0;
 
+  // --------------------------------------------------------------- //
   // Define a function to format the date
   function formatDate(date) {
     const options = { day: "numeric", month: "long", year: "numeric" };
@@ -23,33 +23,31 @@ const App = () => {
   const currentDate = new Date();
   const formattedDate = formatDate(currentDate);
 
+  // --------------------------------------------------------------- //
+  // Function to calculate the difference in days between two dates
   function dateDiffInDays(dateString1, dateString2) {
     const date1 = new Date(dateString1);
     const date2 = new Date(dateString2);
-
-    // Calculate the time difference in milliseconds
     const timeDiff = date2 - date1;
-
-    // Convert milliseconds to days
     const daysDiff = timeDiff / (1000 * 3600 * 24);
-
-    return Math.abs(Math.floor(daysDiff)); // Use Math.abs to get a positive value
+    return Math.abs(Math.floor(daysDiff));
   }
-
   const startDate = new Date("October 07, 2023");
-
   day_num = Math.ceil(dateDiffInDays(startDate, currentDate) + 1);
 
+  // --------------------------------------------------------------- //
+  // Define a function to format the time
   const options = {
-    hour: "numeric", // "numeric", "2-digit"
-    minute: "numeric", // "numeric", "2-digit"
-    hour12: true, // true or false
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
   };
+  let currentTime = new Intl.DateTimeFormat("en-US", options).format(currentDate);
 
+  // --------------------------------------------------------------- //
   // Define a function to update drop information
   const updateDrop = async (name, count, showTime) => {
     try {
-      // Send a POST request to the back-end to update drop information
       console.log("POST Request", name, count, showTime);
       await Axios.post(`${baseURL}/update-drop`, { name, count, formattedDate });
       await Axios.post(`${baseURL}/update-showtime`, { showTime: showTime });
@@ -60,31 +58,34 @@ const App = () => {
     }
   };
 
+  // --------------------------------------------------------------- //
   // Define a function to fetch drop information
   const fetchShowTime = async () => {
     try {
       const response = await Axios.get(`${baseURL}/get-showtime`);
       console.log(response.data.time);
-      return response.data.time; // Assuming the response contains the showTime
+      return response.data.time;
     } catch (error) {
       alert("Error fetching showTime:", error);
       return "";
     }
   };
 
+  // --------------------------------------------------------------- //
   // Define a function to fetch drop information
   const fetchDropInfo = async (formattedDate) => {
     try {
-      // Send a GET request to the back-end to fetch drop information
       console.log(formattedDate);
       const response = await Axios.get(`${baseURL}/get-drop-info?formattedDate=${formattedDate}`);
-      return response.data; // Assuming the response contains drop information
+      return response.data;
     } catch (error) {
       alert("Error fetching drop information:", error);
-      return []; // Return an empty array in case of an error
+      return [];
     }
   };
 
+  // --------------------------------------------------------------- //
+  // Define a function to create a drop
   const createDrop = async () => {
     try {
       await Axios.get(`${baseURL}/create-drop`);
@@ -95,9 +96,9 @@ const App = () => {
     }
   };
 
+  // --------------------------------------------------------------- //
   // Use Effect to fetch the data
   useEffect(() => {
-    // Fetch drop information when the component mounts
     Axios.get(`${baseURL}/create-drop`);
     fetchDropInfo(formattedDate).then((data) => setDropInfo(data));
     fetchShowTime().then((data) => setShowtime(data));
@@ -118,7 +119,7 @@ const App = () => {
 
       <main className="App-main">
         {dropInfo.length === 0 ? (
-          <button className="create-button" onClick={createDrop()}>
+          <button className="create-button" onClick={createDrop}>
             Create
           </button>
         ) : (
@@ -146,21 +147,7 @@ const App = () => {
               return null;
             } else if (day_num > 2 && info.name === "Milflox") {
               return null;
-            } else
-              return (
-                <Sections
-                  key={index}
-                  times={times}
-                  name={info.name}
-                  imgUrl={imgUrl}
-                  done={info.count}
-                  buttonClick={() => {
-                    const updatedTime = new Intl.DateTimeFormat("en-US", options).format(new Date());
-                    setCurrentTime(updatedTime);
-                    updateDrop(info.name, info.count + 1, currentTime);
-                  }}
-                />
-              );
+            } else return <Sections key={index} times={times} name={info.name} imgUrl={imgUrl} done={info.count} buttonClick={() => updateDrop(info.name, info.count + 1, currentTime)} />;
           })
         )}
       </main>
